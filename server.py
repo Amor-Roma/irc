d41629b4501c647533a21f396039d8584d3a2f28
amor_roma <chrisroma23@gmail.com>
2015-03-15 16:07:06 +0000

    cloud9 changes

--- server.py
+++ server.py
@@ -5,9 +5,12 @@ import psycopg2.extras
 from flask import Flask, session
 from flask.ext.socketio import SocketIO, emit
 
+serialID = 0
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'secret!'
+app.secret_key = os.urandom(24).encode('hex')
+
 
 socketio = SocketIO(app)
 
@@ -17,9 +20,11 @@ users = {}
 def connectToDB():
   connectionString = 'dbname=irc user=postgres password=Amorroma host=localhost'
   try:
+    print "connected to database IRC"
     return psycopg2.connect(connectionString)
   except:
     print("Can't connect to database")
+    
 
 def updateRoster():
     names = []
@@ -38,7 +43,6 @@ def test_connect():
     session['uuid']=uuid.uuid1()
     session['username']='starter name'
     print 'connected'
-    
     users[session['uuid']]={'username':'New User'}
     updateRoster()
 
@@ -52,6 +56,17 @@ def new_message(message):
     tmp = {'text':message, 'name':users[session['uuid']]['username']}
     messages.append(tmp)
     emit('message', tmp, broadcast=True)
+    #######################################################start here with message sql queries
+    query = "insert into data (id, message) values (%s, %s);"
+    #query = "insert into data (id, message) values (" + serialID + ", " + message + ");"
+    print serialID
+    try:
+        cur.execute(query, (serialID, message))
+        #cur.execute(query)
+    except:
+        print("Error executing query")
+        print query
+    
     
 @socketio.on('identify', namespace='/chat')
 def on_identify(message):
@@ -61,8 +76,18 @@ def on_identify(message):
 
 
 @socketio.on('login', namespace='/chat')
-def on_login(pw):
-    print 'login '  + pw
+def on_login(data):
+    print data['name'] + " " +  data['password']
+    results = ''
+    query = "select id from users WHERE name = %s AND password = crypt(%s, password);"
+    try:
+        cur.execute(query, (data['name'], data['password']))
+        session['serialID'] = cur.fetchone()
+        print session['serialID']
+        print serialID
+    except:
+        print("Error executing query")
+        print query
     #users[session['uuid']]={'username':message}
     #updateRoster()
 
@@ -79,7 +104,6 @@ def on_disconnect():
 def hello_world():
     print 'in hello world'
     return app.send_static_file('index.html')
-    return 'Hello World!'
 
 @app.route('/js/<path:path>')
 def static_proxy_js(path):
@@ -97,7 +121,8 @@ def static_proxy_img(path):
     return app.send_static_file(os.path.join('img', path))
     
 if __name__ == '__main__':
-    print "A"
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
      
